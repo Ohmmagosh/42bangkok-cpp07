f16293a9a9ac8e1fea58213601879260fd65d5a7
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-06-14 23:24:06 +0700

    ADD ex02 final

---
+++ ex02/Array.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 16:30:58 by psuanpro          #+#    #+#             */
+/*   Updated: 2023/06/14 23:11:33 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+template <typename T>
+
+class Array {
+	public:
+		Array( void ): _size(0), _data(NULL){
+			std::cout << "Empty Array created!!" << std::endl;
+		};
+		Array( const unsigned int n ): _size(n) {
+				std::cout << "Copy constructor created!!" << std::endl;
+				_data = new T[_size];
+		};
+		Array<T>( const Array<T>& cp ): _size(cp._size), _data(new T[cp._size]){
+			for (unsigned int i = 0; i < _size; i++){
+				_data[i] = cp._data[i];
+			}
+		}
+		Array<T>& operator=( const Array<T>& cp ){
+			if (this != &cp) {
+				delete [] _data;
+				_size = cp._size;
+				_data = new T[_size];
+				for (unsigned int i = 0; i < _size; i++) {
+					_data[i] = cp._data[i];
+				}
+			}
+			return (*this);
+		};
+		T&	operator[](const unsigned int index) {
+			if (index < 0 || _size < 0)
+				throw std::overflow_error("index is out of bounds");
+			else
+				return _data[index];
+		};
+		unsigned int	size(void){
+			return _size;
+		};
+		~Array(){
+			delete [] _data;
+			std::cout << "Array destroy!!" << std::endl;
+		};
+
+	private:
+		unsigned int	_size;
+		T*				_data;
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 13:52:30 by psuanpro          #+#    #+#             */
+/*   Updated: 2023/06/14 23:23:05 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int	main(void) {
+	try {
+		Array<int>	a;
+		Array<int>	b(3);
+		Array<int>	c(-3);
+
+		for (unsigned int i = 0; i < b.size(); i++) {
+			b[i] = i;
+		}
+		for (unsigned int i = 0; i < b.size(); i++) {
+			std::cout << "b["<< i << "] = " << b[i] << std::endl;
+		}
+		a = b;
+		std::cout << std::endl;
+		for (unsigned int i = 0; i < a.size(); i++) {
+			std::cout << "a["<< i << "] = " << a[i] << std::endl;
+		}
+
+		std::cout << std::endl;
+		std::cout << "a[1] = "<< a[1] << std::endl;
+
+		std::cout << std::endl;
+		Array<int> d(b);
+		for (unsigned int i = 0; i < d.size(); i++) {
+			std::cout << "d["<< i << "] = " << d[i] << std::endl;
+		}
+		Array<char> e(3);
+		e[0] = 'a';
+		e[1] = 'b';
+		e[2] = 'c';
+		for (unsigned int i = 0; i < e.size(); i++) {
+			std::cout << "e["<<i<<"] = "<< e[i] << std::endl;
+		}
+
+		Array<std::string> f(3);
+		f[0] = "hello";
+		f[1] = "world";
+		f[2] = "42";
+		for (unsigned int i = 0; i < f.size(); i++) {
+			std::cout << "f["<<i<<"] = "<< f[i] << std::endl;
+		}
+	} catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+
+	return (0);
+}
