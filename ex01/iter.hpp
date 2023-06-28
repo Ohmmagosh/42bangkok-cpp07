28bd90cb33673516d177a50284d8be8ba5a5cc05
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-06-28 17:12:48 +0700

    ADD new main ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:52:32 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/06/14 22:22:55 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/06/28 03:50:00 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 # include <iostream>
 
-template <typename T> void iter(T *array, int length, void (*f)(T &)) {
+template <typename T>
+void iter(T *array, int length, void (*f)(const T&)) {
+	if (array == NULL || f == NULL)
+		return ;
 	for (int i = 0; i < length;i++)
 		f(array[i]);
 };
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,18 +6,56 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:52:30 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/06/14 16:22:28 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/06/28 03:49:16 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void	say(int& v) {
-	std::cout << v;
+// void	say(int& v) {
+// 	std::cout << v;
+// }
+
+// int main(void) {
+// 	int	ar[]={1, 2, 3, 4, 5, 6};
+// 	iter(ar, sizeof(ar) / sizeof(ar[0]), say);
+// 	return (0);
+// }
+
+template <typename T>
+void print(const T& x)
+{
+    std::cout << x << " ";
 }
 
-int main(void) {
-	int	ar[]={1, 2, 3, 4, 5, 6};
-	::iter(ar, sizeof(ar) / sizeof(ar[0]), say);
-	return (0);
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
